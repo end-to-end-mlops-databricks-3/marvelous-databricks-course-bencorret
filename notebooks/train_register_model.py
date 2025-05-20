fbf9478b9a11d183e759a0abcf86f79931d97d01
bencorret <bencorret>
2025-05-20 16:37:43 +0000

    Adding base procedural steps in notebook

--- notebooks/train_register_model.py
+++ notebooks/train_register_model.py
@@ -1,10 +1,13 @@
 # Databricks notebook source
 
-# COMMAND ----------
-# %pip install -e ..
 
 # COMMAND ----------
-# %restart_python
+
+# MAGIC %pip install -e ..
+
+# COMMAND ----------
+
+# MAGIC %restart_python
 
 # COMMAND ----------
 
@@ -24,6 +27,7 @@ from marvelous.timer import Timer
 
 from us_accidents.config import ProjectConfig
 from us_accidents.data_processor import DataProcessor
+# from us_accidents.models.model import xxx
 
 # Load configuration
 config_path = os.path.abspath(os.path.join(Path.cwd(), "..", "project_config.yaml"))
@@ -34,3 +38,41 @@ setup_logging(log_file="logs/marvelous-1.log")
 logger.info("Configuration loaded:")
 logger.info(yaml.dump(config, default_flow_style=False))
 
+
+# COMMAND ----------
+
+1
+
+# COMMAND ----------
+
+# Generate two DFs from our training and testing datasets
+
+# COMMAND ----------
+
+# Set tracking uri, to be able to work locally too
+
+# COMMAND ----------
+
+# Create experiment: mlflow.set_experiment()
+# Add tag to experiment: mlflow.set_experiment_tags() / or add tags: {"yyy": "xxx"} to experiment. Add git commit hash? And more?
+
+# COMMAND ----------
+
+# Start a run, with a WITH statement
+# Logg metrics, list of column ... Or perhaps better: mlflow.<model_flavor>.autolog()
+
+# Log the input too, very important: mlflow.log_input()
+# When the model is created, also log the signature!
+# To log the model: mlflow.<model_flavor>.log_model()
+
+# COMMAND ----------
+
+# Fetch our experiment using a name and tag
+# Fund the latest run_id (we trust it's the best)
+# Find the logged model
+
+# COMMAND ----------
+
+# Register model
+# Two ways. With mlflow.register_model(), we can pass tags
+# And it seems
