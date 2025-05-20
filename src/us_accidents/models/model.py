3be12bdb4e4a76f1aecf0d8e5e45faa92d55388c
BenCo <corretconseil@outlook.com>
2025-05-20 22:28:08 +0200

    Worked on model class, ready for notebook work

---
+++ src/us_accidents/models/model.py
@@ -0,0 +1,184 @@
+"""Basic model implementation.
+
+infer_signature (from mlflow.models) → Captures input-output schema for model tracking.
+
+num_features → List of numerical feature names.
+cat_features → List of categorical feature names.
+target → The column to predict.
+parameters → Hyperparameters for RandomForestClassifier.
+catalog_name, schema_name → Database schema names for Databricks tables.
+"""
+
+import mlflow
+import numpy as np
+import pandas as pd
+from loguru import logger
+from mlflow import MlflowClient
+from mlflow.data.dataset_source import DatasetSource
+from mlflow.models import infer_signature
+from pyspark.sql import SparkSession
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+from us_accidents.config import ProjectConfig, Tags
+
+
+class BasicModel:
+    """A basic model class for accident severity in the US using a radom forest classifier.
+
+    This class handles data loading, model training, and MLflow logging.
+    """
+
+    def __init__(self, config: ProjectConfig, tags: Tags, spark: SparkSession) -> None:
+        """Initialize the model with project configuration.
+
+        :param config: Project configuration object
+        :param tags: Tags object
+        :param spark: SparkSession object
+        """
+        self.config = config
+        self.spark = spark
+
+        # Extract settings from the config
+        self.num_features = self.config.num_features
+        self.cat_features = self.config.cat_features
+        self.target = self.config.target
+        self.parameters = self.config.parameters
+        self.catalog_name = self.config.catalog_name
+        self.schema_name = self.config.schema_name
+        self.experiment_name = self.config.experiment_name
+        self.model_name = f"{self.catalog_name}.{self.schema_name}.us_accidents_model"
+        self.tags = tags.dict()
+
+    def load_data(self) -> None:
+        """Load training and testing data from Delta tables.
+
+        Splits data into features (X_train, X_test) and target (y_train, y_test).
+        """
+        logger.info("🔄 Loading data from Databricks tables...")
+        self.train_set_spark = self.spark.table(f"{self.catalog_name}.{self.schema_name}.train_set")
+        self.train_set = self.train_set_spark.toPandas()
+        self.test_set = self.spark.table(f"{self.catalog_name}.{self.schema_name}.test_set").toPandas()
+        self.data_version = "0"  # describe history -> retrieve
+
+        self.X_train = self.train_set[self.num_features + self.cat_features]
+        self.y_train = self.train_set[self.target]
+        self.X_test = self.test_set[self.num_features + self.cat_features]
+        self.y_test = self.test_set[self.target]
+        logger.info("✅ Data successfully loaded.")
+
+    def train(self) -> None:
+        """Train the model."""
+
+        logger.info("🔄 Building random forest classifier...")
+        clf_base = RandomForestClassifier()
+        grid = {'n_estimators': self.parameters['n_estimators'],
+                'max_features': self.parameters['max_features'],}
+        self.clf_rf = GridSearchCV(clf_base, 
+                                   grid, 
+                                   cv=self.parameters['cv'], 
+                                   n_jobs=self.parameters['n_jobs'], 
+                                   scoring=self.parameters['scoring'],)
+        
+        logger.info("🚀 Starting training...")
+        self.clf_rf.fit(self.X_train, self.y_train)
+        logger.info("✅ Completed training...")
+
+    def log_model(self) -> None:
+        """Log the model using MLflow."""
+        mlflow.set_experiment(self.experiment_name)
+        with mlflow.start_run(tags=self.tags) as run:
+            self.run_id = run.info.run_id
+
+            y_pred = self.clf_rf.predict(self.X_test)
+
+            # Evaluate metrics
+            mse = mean_squared_error(self.y_test, y_pred)
+            mae = mean_absolute_error(self.y_test, y_pred)
+            r2 = r2_score(self.y_test, y_pred)
+
+            logger.info(f"📊 Mean Squared Error: {mse}")
+            logger.info(f"📊 Mean Absolute Error: {mae}")
+            logger.info(f"📊 R2 Score: {r2}")
+
+            # Log parameters and metrics
+            mlflow.log_param("model_type", "RandomForestClassifier")
+            mlflow.log_params(self.parameters)
+            mlflow.log_metric("mse", mse)
+            mlflow.log_metric("mae", mae)
+            mlflow.log_metric("r2_score", r2)
+
+            # Log the model
+            signature = infer_signature(model_input=self.X_train, model_output=y_pred)
+            dataset = mlflow.data.from_spark(
+                self.train_set_spark,
+                table_name=f"{self.catalog_name}.{self.schema_name}.train_set",
+                version=self.data_version,
+            )
+            mlflow.log_input(dataset, context="training")
+            mlflow.sklearn.log_model(
+                sk_model=self.pipeline, artifact_path="random-forest-classifier-model", signature=signature
+            )
+
+    def register_model(self) -> None:
+        """Register model in Unity Catalog."""
+        logger.info("🔄 Registering the model in UC...")
+        registered_model = mlflow.register_model(
+            model_uri=f"runs:/{self.run_id}/random-forest-classifier-model",
+            name=self.model_name,
+            tags=self.tags,
+        )
+        logger.info(f"✅ Model registered as version {registered_model.version}.")
+
+        latest_version = registered_model.version
+
+        client = MlflowClient()
+        client.set_registered_model_alias(
+            name=self.model_name,
+            alias="latest-model",
+            version=latest_version,
+        )
+
+    def retrieve_current_run_dataset(self) -> DatasetSource:
+        """Retrieve MLflow run dataset.
+
+        :return: Loaded dataset source
+        """
+        run = mlflow.get_run(self.run_id)
+        dataset_info = run.inputs.dataset_inputs[0].dataset
+        dataset_source = mlflow.data.get_source(dataset_info)
+        logger.info("✅ Dataset source loaded.")
+        return dataset_source.load()
+
+    def retrieve_current_run_metadata(self) -> tuple[dict, dict]:
+        """Retrieve MLflow run metadata.
+
+        :return: Tuple containing metrics and parameters dictionaries
+        """
+        run = mlflow.get_run(self.run_id)
+        metrics = run.data.to_dictionary()["metrics"]
+        params = run.data.to_dictionary()["params"]
+        logger.info("✅ Dataset metadata loaded.")
+        return metrics, params
+
+    def load_latest_model_and_predict(self, input_data: pd.DataFrame) -> np.ndarray:
+        """Load the latest model from MLflow (alias=latest-model) and make predictions.
+
+        Alias latest is not allowed -> we use latest-model instead as an alternative.
+
+        :param input_data: Pandas DataFrame containing input features for prediction.
+        :return: Pandas DataFrame with predictions.
+        """
+        logger.info("🔄 Loading model from MLflow alias 'production'...")
+
+        model_uri = f"models:/{self.model_name}@latest-model"
+        model = mlflow.sklearn.load_model(model_uri)
+
+        logger.info("✅ Model successfully loaded.")
+
+        # Make predictions
+        predictions = model.predict(input_data)
+
+        # Return predictions as a DataFrame
+        return predictions
